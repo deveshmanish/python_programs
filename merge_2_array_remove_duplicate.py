f8007f9905943ca4af4dbf49b168e77435e034e1
Devesh Chaturvedi <deveshmanish@gmail.com>
2022-07-09 20:47:23 +0530

    Added 1 more programs

---
+++ merge_2_array_remove_duplicate.py
@@ -0,0 +1,18 @@
+def merger(a,b):
+    result = []
+    j = 0
+    if len(b)<len(a):
+        (a,b) = (b,a)
+    for i in range(len(a)):
+        if a[i]<b[j]:
+            if not result:
+                result.append(a[i])
+            elif a[i]!=result[-1]:
+                result.append(a[i])
+        else:
+            if not result:
+                result.append(b[j])
+            elif result and b[j]!=result[-1]:
+                result.append(b[j])
+            j += 1
+    result += b[len(a):]
