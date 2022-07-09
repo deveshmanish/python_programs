9b04c6fd45a3a737900e2334644bcd114b3d4e83
Devesh Chaturvedi <deveshmanish@gmail.com>
2022-07-09 20:17:16 +0530

    Added 2 more programs

--- sort_positive_and_negative_elements_sorted_array.py
+++ sort_positive_and_negative_elements_sorted_array.py
@@ -1,11 +1,8 @@
 def sortArray(a):
     i = 0
     for j in range(len(a)-1,i,-1):
-        if abs(a[i])>abs(a[i+1]):
-            if abs(a[i])>=abs(a[j]):
-                (a[i],a[j]) = (a[j],a[i])
-        else:
-            (a[i],a[i+1]) = (a[i+1],a[i])
-            if abs(a[i])>=abs(a[j]):
-                (a[i],a[j]) = (a[j],a[i])
+        if abs(a[i])<=abs(a[i+1]):
+            (a[i], a[i + 1]) = (a[i + 1], a[i])
+        if abs(a[i])>=abs(a[j]):
+            (a[i],a[j]) = (a[j],a[i])
     return a
