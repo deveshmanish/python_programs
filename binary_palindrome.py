4c9bc55789b6cfb5a7d2f2c9759eebdf1fbff9fb
Devesh Chaturvedi <deveshmanish@gmail.com>
2022-07-09 23:48:01 +0530

    Added 1 program

---
+++ binary_palindrome.py
@@ -0,0 +1,12 @@
+def checkPalindrome(number):
+    binary = "{0:b}".format(int(number))
+    (i,j) = (0,len(binary)-1)
+    while binary[j]=='0':
+        j -= 1
+    while j >= i:
+        if binary[i] == binary[j]:
+            i += 1
+            j -= 1
+        else:
+            return False
+    return True
