a1ea3117656611a52ba434686f1bf0124563e918
Devesh Chaturvedi <deveshmanish@gmail.com>
2022-07-09 17:51:54 +0530

    Added 2 more programs

---
+++ 1d_sort_no_duplicate_m_by_n_row_sorted_array.py
@@ -0,0 +1,22 @@
+def sort(array):
+    index_array = [0 for i in range(len(array))]
+    result = []
+    starting_row = 0
+    while(sum(index_array)!=len(array)*len(array[0])):
+        lowest_indices = []
+        lowest = array[starting_row][index_array[starting_row]]
+        for i in range(len(array)):
+            if index_array[i]<len(array[0]):
+                if array[i][index_array[i]]<=lowest:
+                    lowest = array[i][index_array[i]]
+                    lowest_indices.append(i)
+        result.append(lowest)
+        for ind in lowest_indices:
+            if array[ind][index_array[ind]]==lowest and index_array[ind]<len(array[0]):
+                index_array[ind] += 1
+        while not index_array[starting_row]<(len(array[0])):
+            if starting_row<len(array):
+                starting_row += 1
+            if sum(index_array)==len(array)*len(array[0]):
+                break
+    return result
